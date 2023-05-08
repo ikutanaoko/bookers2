f93815e3da417910cca830f229d9b9d46aac8340
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-05-08 09:19:12 +0000

    05081818

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,29 @@
 class BooksController < ApplicationController
 
   def index
+    @user = User.find(current_user.id)
     @book = Book.new
     @books = Book.all
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = User.find(@book.user.id)
+    @books = @user.books
+    @book_new = Book.new
+
   end
   
   def create
+    @user = User.find(current_user.id)
+    @books = Book.all
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
-
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end  
   
   def edit
@@ -23,9 +32,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
     
   end
 
   def index
-    @users =User.all
+    @user = User.find(current_user.id)
     @book =Book.new
+    @users =User.all
+    
+    
   end
 
   def edit
@@ -16,8 +20,11 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(params[:id])
+    else
+      render :edit
+    end
   end
   
   private
